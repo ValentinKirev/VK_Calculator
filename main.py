bd20609e5842b17d7b31a3dec134e060c91d6bf9
Valentin Kirev <valentinkirev666@gmail.com>
2023-09-24 19:59:22 +0300

    added class Calculator with simple calculation functionalities

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from utils import center_window
+from utils import center_window, Calculator
 from PIL import Image, ImageTk
 
 root = tk.Tk()
@@ -10,70 +10,90 @@ root.geometry(center_window(root, 350, 428))
 ico = Image.open('logo.jpg')
 photo = ImageTk.PhotoImage(ico)
 root.wm_iconphoto(False, photo)
-equation = tk.StringVar()
 
-expression_field = tk.Entry(root, textvariable=equation, bg="#c4abab", font="sans 16 bold")
+calculator = Calculator()
+
+expression_field = tk.Entry(root, textvariable=calculator.equation, bg="#c4abab", font="sans 16 bold")
 expression_field.grid(columnspan=4, ipadx=54, ipady=20)
 
-button_clear = tk.Button(root, text=" clear ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_clear = tk.Button(root, text=" clear ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3,
+                         borderwidth=5, relief="raised", command=calculator.clear)
 button_clear.grid(row=2, column=0)
 
-button_percentage = tk.Button(root, text=" % ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_percentage = tk.Button(root, text=" % ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3,
+                              borderwidth=5, relief="raised")
 button_percentage.grid(row=2, column=1)
 
-button_power = tk.Button(root, text=" x\u00b2 ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_power = tk.Button(root, text=" x\u00b2 ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3,
+                         borderwidth=5, relief="raised")
 button_power.grid(row=2, column=2)
 
-button_plus = tk.Button(root, text=" + ", fg="black", bg="#14e3a5", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_plus = tk.Button(root, text=" + ", fg="black", bg="#14e3a5", font="sans 11 bold", width=8, height=3,
+                        borderwidth=5, relief="raised", command=lambda: calculator.press_button(button_plus))
 button_plus.grid(row=2, column=3)
 
-button1 = tk.Button(root, text=" 1 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button1 = tk.Button(root, text=" 1 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button1))
 button1.grid(row=3, column=0)
 
-button2 = tk.Button(root, text=" 2 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button2 = tk.Button(root, text=" 2 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button2))
 button2.grid(row=3, column=1)
 
-button3 = tk.Button(root, text=" 3 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button3 = tk.Button(root, text=" 3 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button3))
 button3.grid(row=3, column=2)
 
-button_minus = tk.Button(root, text=" - ", fg="black", bg="#14e3a5", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_minus = tk.Button(root, text=" - ", fg="black", bg="#14e3a5", font="sans 11 bold", width=8, height=3,
+                         borderwidth=5, relief="raised", command=lambda: calculator.press_button(button_minus))
 button_minus.grid(row=3, column=3)
 
-button4 = tk.Button(root, text=" 4 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button4 = tk.Button(root, text=" 4 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button4))
 button4.grid(row=4, column=0)
 
-button5 = tk.Button(root, text=" 5 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button5 = tk.Button(root, text=" 5 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button5))
 button5.grid(row=4, column=1)
 
-button6 = tk.Button(root, text=" 6 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button6 = tk.Button(root, text=" 6 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button6))
 button6.grid(row=4, column=2)
 
-button_multiply = tk.Button(root, text=" * ", fg="black", bg="#14e3a5", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_multiply = tk.Button(root, text=" * ", fg="black", bg="#14e3a5", font="sans 11 bold", width=8, height=3,
+                            borderwidth=5, relief="raised", command=lambda: calculator.press_button(button_multiply))
 button_multiply.grid(row=4, column=3)
 
-button7 = tk.Button(root, text=" 7 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button7 = tk.Button(root, text=" 7 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button7))
 button7.grid(row=5, column=0)
 
-button8 = tk.Button(root, text=" 8 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button8 = tk.Button(root, text=" 8 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button8))
 button8.grid(row=5, column=1)
 
-button9 = tk.Button(root, text=" 9 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button9 = tk.Button(root, text=" 9 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button9))
 button9.grid(row=5, column=2)
 
-button_divide = tk.Button(root, text=" / ", fg="black", bg="#14e3a5", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_divide = tk.Button(root, text=" / ", fg="black", bg="#14e3a5", font="sans 11 bold", width=8, height=3,
+                          borderwidth=5, relief="raised", command=lambda: calculator.press_button(button_divide))
 button_divide.grid(row=5, column=3)
 
-button_clear = tk.Button(root, text=" +/- ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
-button_clear.grid(row=6, column=0)
+button_sign_change = tk.Button(root, text=" +/- ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3,
+                               borderwidth=5, relief="raised")
+button_sign_change.grid(row=6, column=0)
 
-button0 = tk.Button(root, text=" 0 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button0 = tk.Button(root, text=" 0 ", fg="black", bg="#c79e0c", font="sans 11 bold", width=8, height=3,
+                    borderwidth=5, relief="raised", command=lambda: calculator.press_button(button0))
 button0.grid(row=6, column=1)
 
-button_decimal = tk.Button(root, text=" . ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_decimal = tk.Button(root, text=" . ", fg="black", bg="#d914e3", font="sans 11 bold", width=8, height=3,
+                           borderwidth=5, relief="raised", command=lambda: calculator.press_button(button_decimal))
 button_decimal.grid(row=6, column=2)
 
-button_equal = tk.Button(root, text=" = ", fg="black", bg="#1905fa", font="sans 11 bold", width=8, height=3, borderwidth=5, relief="raised")
+button_equal = tk.Button(root, text=" = ", fg="black", bg="#1905fa", font="sans 11 bold", width=8, height=3,
+                         borderwidth=5, relief="raised", command=calculator.calculate)
 button_equal.grid(row=6, column=3)
 
 root.mainloop()
-
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import re
+import tkinter as tk
+
+
 def center_window(root, window_width, window_height):
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -6,3 +10,45 @@ def center_window(root, window_width, window_height):
     y_coordinate = int((screen_height / 2) - (window_height / 2))
 
     return f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}"
+
+
+class Calculator:
+    def __init__(self):
+        self.expression = ""
+        self.equation = tk.StringVar()
+        self.signs = ["+", "-", "/", "*", "."]
+
+    def set_equation(self):
+        self.equation.set(self.expression)
+
+    def calculate(self):
+        if self.expression[-1] not in self.signs:
+            try:
+                self.expression = str(eval(self.expression))
+            except SyntaxError:
+                self.expression = "error"
+            finally:
+                self.set_equation()
+
+    def clear(self):
+        self.expression = ""
+        self.set_equation()
+
+    def press_button(self, button):
+        expressions = re.split(r'[-/+*]', self.expression)
+
+        button_text = button.cget("text").strip()
+
+        if button_text == "." and "." in expressions[-1]:
+            self.expression = self.expression
+        else:
+            if len(self.expression) == 0 and button_text in self.signs \
+                    or button_text == "." and self.expression[-1] in self.signs \
+                    or button_text == "0" and len(expressions[-1]) == 1 and expressions[-1] == "0":
+                self.expression = self.expression
+            elif len(self.expression) > 0 and self.expression[-1] in self.signs and button_text in self.signs:
+                self.expression = self.expression[:-1] + button_text
+            else:
+                self.expression += button_text
+
+        self.set_equation()
